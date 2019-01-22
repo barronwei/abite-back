4c889ef9307230d53096becfa0b537d86366b24c
josephokeefe <josephokeefe@college.harvard.edu>
2019-01-22 16:03:40 -0500

    migration update

--- db/migrations/20181127194722_create-uuid.js
+++
@@ -1,7 +0,0 @@
-exports.up = async function(knex, Promise) {
-  await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-}
-
-exports.down = async function(knex, Promise) {
-  await knex.schema.raw('DROP EXTENSION "uuid-ossp";')
-}
