25b84f4de6260535941d7e4dd1b224a7ae3d688e
Leon Yang <leonyang@college.harvard.edu>
2019-01-23 10:42:33 -0500

    migrations

--- db/migrations/20190122160303_add-user.js
+++ db/migrations/20190122160303_add-user.js
@@ -1,3 +1,23 @@
-exports.up = function(knex, Promise) {}
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', table => {
+    table
+      .uuid('id')
+      .notNull()
+      .primary()
+    table.text('name').notNull()
+    table.text('email').notNull()
+    table.text('hometown').notNull()
+    table
+      .timestamp('createdAt')
+      .defaultTo(knex.fn.now())
+      .notNull()
+    table
+      .timestamp('updatedAt')
+      .defaultTo(knex.fn.now())
+      .notNull()
+  })
+}
 
-exports.down = function(knex, Promise) {}
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+}
